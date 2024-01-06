50c21530e2161055b5932aac0ac4469db7e7552d
Alan Gardiner <kwarouag@gmail.com>
2024-01-06 22:32:48 +0000

    Coded the input that allows the app to run the queries for listing albums or artists

--- app.py
+++ app.py
@@ -25,29 +25,30 @@ class Application():
                            '2 - List all artists\n'
                            'Enter your choice: '))
         
+        # My comment:
+        # Create an instance of the class ArtistReposistory 
+        # Creates a connection between the Artist Repository Class Object and the database so that the data is accesible.
+        artist_repository = ArtistRepository(self._connection)
+        album_repository = AlbumRepository(self._connection)
+
         if user_input == 1:
             # Retrieve all artists
-            # My comment:
-            # Create an instance of the class ArtistReposistory 
-            # Creates a connection between the Artist Repository Class Object and the database so that the data is accesible.
             # Calls the all method and stores the list of artist data as individual objects from the artist class in a variable
-            artist_repository = ArtistRepository(self._connection)
             artists = artist_repository.all()
             # List them out
             for artist in artists:
                 print(artist)
 
-
+        if user_input == 2:
+            albums = album_repository.all()
+            for album in albums:
+                print(album)
             
 if __name__ == '__main__':
     app = Application()
     app.run()
 
 '''
-album_repository = AlbumRepository(connection)
-
-
-
 album = album_repository.find(1)
 print(album)
 
@@ -57,9 +58,8 @@ for album in albums:
     print(album)
 
 delete_album = album_repository.delete(13)
-albums = album_repository.all()
-for album in albums:
-    print(album)
+
+
 '''
     
 """
