cff9e6daa62ac53300b2489cb9bd0a8a50f69210
Alan Gardiner <kwarouag@gmail.com>
2024-01-06 18:38:48 +0000

    Wrote the test for creating new album record in database

--- tests/test_album_repository.py
+++ tests/test_album_repository.py
@@ -25,7 +25,6 @@ def test_get_all_records(db_connection):
         Album(10, "Here Comes the Sun", 1971, 4),
         Album(11, "Fodder on My Wings", 1982, 4),
         Album(12, "Ring Ring", 1973, 2),
-        Album(13, "ABBA", 1968, 2)
     ]
 
 def test_find_album_by_id(db_connection):
@@ -35,3 +34,30 @@ def test_find_album_by_id(db_connection):
     album = respository.find(2)
 
     assert album == Album(2, "Surfer Rosa", 1988, 1)
+
+"""
+When we call AlbumRepository#create
+We get a new record in the database
+"""
+def test_create_album(db_connection):
+    db_connection.seed("seeds/music_library.sql")
+    repository = AlbumRepository(db_connection)
+
+    repository.create(Album(None,"Trome Le Monde", 1991, 1))
+
+    result = repository.all()
+    assert result == [
+        Album(1, 'Doolittle', 1989, 1),
+        Album(2,'Surfer Rosa', 1988, 1), 
+        Album(3,'Waterloo', 1974, 2),
+        Album(4,'Super Trouper', 1980, 2), 
+        Album(5,'Bossanova', 1990, 1),
+        Album(6, 'Lover', 2019, 3),
+        Album(7,'Folklore', 2020,3),
+        Album(8,'I Put a Spell on You', 1965, 4),
+        Album(9,'Baltimore', 1978,4), 
+        Album(10,'Here Comes the Sun', 1971, 4), 
+        Album(11,'Fodder on My Wings', 1982, 4),
+        Album(12,'Ring Ring', 1973,2),
+        Album(13,'Trome Le Monde', 1991, 1)
+    ]
