9f9a437d1c26d697bcc24c1b88447e36aadbf483
Alan Gardiner <kwarouag@gmail.com>
2024-01-06 17:47:04 +0000

    Wrote the code for album_repository

---
+++ lib/album_repository.py
@@ -0,0 +1,28 @@
+from lib.album import Album
+
+
+class AlbumRepository:
+    #Database connection
+    def __init__(self, connection):
+        self._connection = connection
+
+    
+    def all(self):
+        """
+        returns a list of album objects:
+        No arguents
+        """
+        rows = self._connection.execute("SELECT * FROM albums")
+
+        albums = []
+        for row in rows:
+            item = Album(row["id"], row["title"], row["release_year"], row["artist_id"])
+            albums.append(item)
+        return albums
+
+    def find(self, album_id):
+
+        rows = self._connection.execute('SELECT * From albums WHERE id = %s', [album_id])
+        row = rows[0]
+        return Album(row["id"], row["title"],  row["release_year"], row["artist_id"])
+    
