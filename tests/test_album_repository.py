ad3c5ee6a51841231af6de51ea898b3b903f9cf8
Alan Gardiner <kwarouag@gmail.com>
2024-01-06 19:06:38 +0000

    Wrote test for Album.delete method

--- tests/test_album_repository.py
+++ tests/test_album_repository.py
@@ -59,5 +59,31 @@ def test_create_album(db_connection):
         Album(10,'Here Comes the Sun', 1971, 4), 
         Album(11,'Fodder on My Wings', 1982, 4),
         Album(12,'Ring Ring', 1973,2),
-        Album(13,'Trome Le Monde', 1991, 1)
+        Album(13,'Trome Le Monde', 1991, 1),
+    ]
+
+def test_delete_album(db_connection):
+    db_connection.seed("seeds/music_library.sql")
+    repository = AlbumRepository(db_connection)
+
+    repository.create(Album(None,"Trome Le Monde", 1991, 1))
+    
+    album_by_id = repository.find(13)
+    assert album_by_id == (Album(13,'Trome Le Monde', 1991, 1))
+
+    repository.delete(13)
+    all_albums = repository.all()
+    assert all_albums == [
+        Album(1, 'Doolittle', 1989, 1),
+        Album(2,'Surfer Rosa', 1988, 1), 
+        Album(3,'Waterloo', 1974, 2),
+        Album(4,'Super Trouper', 1980, 2), 
+        Album(5,'Bossanova', 1990, 1),
+        Album(6, 'Lover', 2019, 3),
+        Album(7,'Folklore', 2020,3),
+        Album(8,'I Put a Spell on You', 1965, 4),
+        Album(9,'Baltimore', 1978,4), 
+        Album(10,'Here Comes the Sun', 1971, 4), 
+        Album(11,'Fodder on My Wings', 1982, 4),
+        Album(12,'Ring Ring', 1973,2),
     ]
