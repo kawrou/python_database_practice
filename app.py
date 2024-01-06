d8cca3cb54a66e5c4763f74e8f1f03920dc5c3c3
Alan Gardiner <kwarouag@gmail.com>
2024-01-06 23:16:40 +0000

    Wrote out all the possible user terminal interactions in the app

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from lib.database_connection import DatabaseConnection
 from lib.artist_repository import ArtistRepository
 from lib.album_repository import AlbumRepository
 from lib.album import Album
+from lib.artist import Artist
 
 class Application():
     def __init__(self):
@@ -21,9 +22,15 @@ class Application():
 
     def run(self):
         user_input = int(input('What would you like to do?\n'
-                           '1 - List all albums\n'
-                           '2 - List all artists\n'
-                           'Enter your choice: '))
+                        '1 - List all artists\n'
+                        '2 - Find artist by ID\n'
+                        '3 - Add new artist\n'
+                        '4 - Delete artist\n'
+                        '5 - List all albums\n'
+                        '6 - Find album by ID \n'
+                        '7 - Add new album\n'
+                        '8 - Delete album by ID\n'
+                        'Enter your choice: '))
         
         # My comment:
         # Create an instance of the class ArtistReposistory 
@@ -40,33 +47,45 @@ class Application():
                 print(artist)
 
         if user_input == 2:
+            artist_id = int(input("Enter artist id: "))
+            artist = artist_repository.find(artist_id)
+            print(artist)
+
+        if user_input == 3:
+            artist_name = input("Enter artist name: ")
+            artist_genre = input("Enter artist's genre: ")
+
+            artist_repository.create(Artist(None, artist_name, artist_genre))
+            print("Artist added succesfully")
+
+        if user_input == 4:
+            artist_id = int(input("Enter artist ID: "))
+            artist_repository.delete(artist_id)
+            print("Artist deleted successfully")
+
+        if user_input == 5:
             albums = album_repository.all()
             for album in albums:
                 print(album)
-            
+
+        if user_input == 6:
+            album_id_search_input = int(input("Enter album id: "))
+            album = album_repository.find(album_id_search_input)
+            print(album)
+
+        if user_input == 7:
+            album_title = input("Enter album title: ")
+            album_release_year = int(input("Enter release year: "))
+            artist_id = int(input("Enter artist ID: "))
+
+            album_repository.create(Album(None,album_title, album_release_year, artist_id))
+            print("Album created successfully")
+
+        if user_input == 8:
+            album_id = int(input("Enter album ID: "))   
+            album_repository.delete(album_id)
+            print("Album deleted successfully") 
+
 if __name__ == '__main__':
     app = Application()
     app.run()
-
-'''
-album = album_repository.find(1)
-print(album)
-
-new_album = album_repository.create(Album(None,"Trome Le Monde", 1991, 1))
-albums = album_repository.all()
-for album in albums:
-    print(album)
-
-delete_album = album_repository.delete(13)
-
-
-'''
-    
-"""
-connection.seed("seeds/book_store.sql")
-book_repository = BookRepository(connection)
-books = book_repository.all()
-
-for book in books:
-    print(book)
-"""
